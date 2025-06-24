d3c7c7fd84fa3636a437abe595f82e1bbc86c59a
kim <elfhoi18@naver.com>
2025-06-24 16:51:04 +0900

    .md file modified commit~

---
+++ src/chapter02/Hello.java
@@ -0,0 +1,9 @@
+package chapter02;
+
+public class Hello {
+	//main metho
+	public static void main(String[] args) {
+		System.out.println("Welcome to JAVA!!");
+		System.out.println("Hello~");
+	}
+}
