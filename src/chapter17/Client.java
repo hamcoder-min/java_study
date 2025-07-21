7a5988ace24737f8f35a0c30100a1c92c5d94678
kim <>
2025-07-21 16:04:35 +0900

    2025-07-21 chapter17 Thread commit!

---
+++ src/chapter17/Client.java
@@ -0,0 +1,104 @@
+package chapter17;
+
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.Frame;
+import java.awt.Panel;
+import java.awt.TextArea;
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+
+public class Client {
+
+	public static void main(String[] args) {
+		try {
+			String id = "이순신";
+			final String HOST = "localhost";
+			final int PORT = 9000;
+			Frame f = new Frame("채팅 클라이언트");
+			Panel p = new Panel();
+			f.setLayout(new BorderLayout());
+			TextArea ta = new TextArea();
+			TextField tf = new TextField(40);
+			Button send = new Button("전송");
+			Button button1 = new Button("종료");
+			ta.setEditable(false);
+			tf.setText("[" + id + "] ");
+			p.add(tf);		p.add(send);
+			
+			Socket s = new Socket(HOST, PORT);	//localhost, 127.0.0.1, 192.168.50.73
+			System.out.println("🧑 클라이언트 접속!");
+			
+			DataInputStream input = new DataInputStream(s.getInputStream());
+			DataOutputStream output = new DataOutputStream(s.getOutputStream());
+			
+			new ServiceThread(input, ta).start();
+//			ta.append(input.readUTF() + "\n");
+			
+//			byte[] buffer = new byte[1024];	//1KB 크기
+//			int bytesRead = input.read(buffer);	//읽은 바이트 수 반환
+//			
+//			if(bytesRead != -1) {
+//				String receivedMessage = new String (buffer, 0, bytesRead, "UTF-8");
+//				ta.append(receivedMessage);
+//			}
+			
+			f.add(p, BorderLayout.NORTH);
+			f.add(ta, BorderLayout.CENTER);
+			f.add(button1, BorderLayout.SOUTH);
+			f.setSize(400, 500);
+			f.setVisible(true);
+			
+			//전송 버튼 이벤트
+			send.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					try {
+						//전송 버튼 클릭 --> 서버로 tf 데이터 전송!!!
+						output.writeUTF(tf.getText()); 
+						tf.setText("[" + id + "] ");
+					} catch (Exception e2) {
+						e2.printStackTrace();
+					}
+				}
+			});
+			
+			
+			//종료 이벤트
+			button1.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					try {
+						System.out.println("-- 클라이언트 종료 --");
+						output.writeUTF("exit");
+						System.exit(0);
+					} catch (Exception e2) {}
+				}
+			});
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
+
+class ServiceThread extends Thread {
+	DataInputStream input;
+	TextArea ta;
+	
+	public ServiceThread(DataInputStream input, TextArea ta) {
+		this.input = input;
+		this.ta = ta;
+	}
+	
+	public void run() {
+		try {
+			while(true) {
+				ta.append(input.readUTF() + "\n");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/chapter17/Server.java
@@ -0,0 +1,79 @@
+package chapter17;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+/**
+ * 멀티 채팅 서버 구현
+ */
+
+public class Server {
+
+	public final static int PORT = 9000;
+	public static ArrayList<ClientHandler> list = new ArrayList<ClientHandler>();
+	
+	public static void main(String[] args) {
+		try {
+			ServerSocket server = new ServerSocket(PORT);
+			System.out.println("서버 실행중 : " + PORT);
+			System.out.println("클라이언트 접속 대기 중~");
+			
+			while(true) {
+				Socket s = server.accept();	//클라이언트 접속 대기 중
+				System.out.println("✅클라이언트 접속!!");
+				
+				//클라이언트 별로 접속하고 별도 종료 진행!!!
+				ClientHandler ch = new ClientHandler(s);
+				list.add(ch);
+				ch.start();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}//main
+
+}//Server
+
+
+class ClientHandler extends Thread {
+	Socket s;
+	DataOutputStream output;
+	DataInputStream input;
+	
+	public ClientHandler(Socket s) {
+		try {
+			this.s = s;
+			this.output = new DataOutputStream(s.getOutputStream());	//전송 	서버가 out을 먼저 만들면 클라이언트는 in을 먼저 만들어야함! out/out 만들면 둘다 보내기만 하고 수신이 없기때문
+			this.input = new DataInputStream(s.getInputStream());		//수신
+			String str = "[서버] 환영합니다~^^";
+			output.writeUTF(str);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void run() {
+		try {
+			boolean flag = true;
+			while(flag) {
+				String receivedMsg = input.readUTF();
+				if(receivedMsg.equals("exit")) {
+					flag = false;
+				} else {
+					Server.list.forEach(ch -> {
+						try {
+							ch.output.writeUTF(receivedMsg);
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					});
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
